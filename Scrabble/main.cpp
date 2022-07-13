466ab78d7d91edf84700ec58f6043d7768dc92c5
James Bankhead <james.bnkhd@gmail.com>
2022-07-13 16:26:36 -0700

    Put solver in seperate thread

--- Scrabble/main.cpp
+++ Scrabble/main.cpp
@@ -9,7 +9,10 @@
 #include <stdarg.h>
 
 #include <algorithm>
+#include <atomic>
 #include <cstring>
+#include <mutex>
+#include <thread>
 
 #include "alphabetset.h"
 #include "board.h"
@@ -21,10 +24,12 @@ struct SolverParams {
     BoardDisplay* boardDisplay;
     Fl_Multiline_Output* statusOutput;
 };
-
-void solve(Fl_Widget* button, void* param) {
-    SolverParams* widgets = reinterpret_cast<SolverParams*>(param);
+std::thread solverThread;
+std::atomic_bool isSolving(false);
+void solve(SolverParams* widgets) {
+    Fl::lock();
     std::string tileString = std::string(widgets->tileInput->value());
+    Fl::unlock();
     char msgBuf[30000];
     msgBuf[0] = '\0';
     // why tf does fltk use c strings? Its a C++ library
@@ -38,8 +43,11 @@ void solve(Fl_Widget* button, void* param) {
         printf("\n");
         vsnprintf(msgBuf + strlen(msgBuf), spaceLeft, msg, args);
         va_end(args);
+        Fl::lock();
         widgets->statusOutput->value(msgBuf);
-        widgets->statusOutput->redraw();
+        Fl::unlock();
+        Fl::awake();
+        isSolving = false;
     };
     // Have to make tiles uppercase
     std::transform(tileString.begin(), tileString.end(), tileString.begin(), ::toupper);
@@ -50,7 +58,7 @@ void solve(Fl_Widget* button, void* param) {
         printMsg("No legal moves found");
         return;
     }
-    printMsg("Found some legal moves", legalMoves.size());
+    printMsg("Found %zu legal moves", legalMoves.size());
     unsigned bestValue = 0;
     Move* bestMove = NULL;
     printMsg("Calculating point values...");
@@ -63,9 +71,21 @@ void solve(Fl_Widget* button, void* param) {
     }
     printMsg("Found \"%s\" for %u points", bestMove->word.c_str(), bestValue);
     board.applyMove(*bestMove);
+    Fl::lock();
     widgets->boardDisplay->redraw();
+    Fl::unlock();
+    Fl::awake();
+}
+
+void solveAsync(Fl_Widget* button, void* param) {
+    if (isSolving) return;
+    SolverParams* widgets = reinterpret_cast<SolverParams*>(param);
+    isSolving = true;
+    if (solverThread.joinable()) solverThread.join();  // join last invocation
+    solverThread = std::thread(solve, widgets);
 }
 int main(int argc, char** argv) {
+    Fl::lock();  // enable threading
     Fl_Window win(900, 450);
     BoardDisplay display(0, 0, win.h(), win.h(), "Display It");
     display.setBoard(&board);
@@ -83,7 +103,7 @@ int main(int argc, char** argv) {
     statusDisplay.value("Ready...");
     controlGroup.end();
     SolverParams params{&tileInput, &display, &statusDisplay};
-    solveButton.callback(solve, &params);
+    solveButton.callback(solveAsync, &params);
     win.end();
     win.show(argc, argv);
     return Fl::run();
